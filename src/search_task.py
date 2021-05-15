b109dac643b0be219f74cc23e18a904e6e731982
mini-hiori <phantombisco@gmail.com>
2021-05-15 23:54:46 +0900

    selectとcreateだいたい完成？

--- src/search_task.py
+++ src/search_task.py
@@ -5,27 +5,26 @@ import boto3
 from typing import List
 
 
-def search_task_by_id(user_id: str, task_id: str) -> List[Task]:
+def search_task_by_id(user_id: str, task_id: str) -> Task:
     """
     user_idとtask_idでtaskを検索する
     """
     dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table("python-todoapi")
-    response = table.query(
+    search_result = table.query(
         KeyConditionExpression=Key("user_id").eq(user_id) & Key("task_id").eq(task_id)
     )
-    if search_result.get("Item"):
+    if search_result.get("Items"):
         # 検索成功
-        task = Task(
-            task_name=search_result["Item"]["task_name"],
-            created_at=search_result["Item"]["created_at"],
-            description=search_result["Item"]["description"],
-            task_id=search_result["Item"]["task_id"],
-        )
-        return [task]
+        result = Task(
+                task_name=search_result["Items"][0]["task_name"],
+                created_at=search_result["Items"][0]["created_at"],
+                description=search_result["Items"][0]["description"],
+                task_id=search_result["Items"][0]["task_id"]
+            )
+        return result
     else:
         return None
-
 
 def search_task_by_name(user_id: int, task_name: str) -> List[Task]:
     """
@@ -35,23 +34,16 @@ def search_task_by_name(user_id: int, task_name: str) -> List[Task]:
     dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table("python-todoapi")
     search_result = table.query(
-        # 設定したLSIのインデックスNameを指定する
-        IndexName="task_name_key",
-        KeyConditionExpression=Key("user_id").eq(user_id)
-        & Key("task_name").begins_with(task_name),
+        #設定したLSIのインデックスNameを指定する
+        IndexName='task_name_key',
+        KeyConditionExpression=Key("user_id").eq(user_id) & Key("task_name").begins_with(task_name)
     )
-    print(search_result)
     if search_result.get("Items"):
         # 検索成功
-        result_list = [
-            Task(
-                task_name=i["task_name"],
-                created_at=i["created_at"],
-                description=i["description"],
-                task_id=i["task_id"],
-            )
-            for i in search_result["Items"]
-        ]
+        result_list = []
+        for i in search_result["Items"]:
+            task = search_task_by_id(user_id,i["task_id"])
+            result_list.append(task)
         return result_list
     else:
         return None
