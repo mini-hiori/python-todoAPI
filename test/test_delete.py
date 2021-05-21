a850255334cf4a0277070af89704704a48f790e4
mini-hiori <phantombisco@gmail.com>
2021-05-21 09:38:11 +0900

    readme修正

--- test/test_delete.py
+++ test/test_delete.py
@@ -10,7 +10,6 @@ def test_delete(task_id: str):
     idtoken = os.environ["IDTOKEN"]
     header = {"Authorization": idtoken}
     target_url = url + f"?task_id={task_id}"
-    print(target_url)
     result = requests.delete(target_url, headers=header)
     print(result.text)
     assert result.status_code == 200, "Error"
