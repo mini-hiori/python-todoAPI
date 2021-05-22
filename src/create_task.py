00651ee5ada1802a621def56b9ccd175a0a425ec
mini-hiori <phantombisco@gmail.com>
2021-05-22 16:02:00 +0900

    try-catch削除

--- src/create_task.py
+++ src/create_task.py
@@ -12,21 +12,17 @@ def create_task(user_id: str, task: InputTask) -> str:
     """
     DynamoDBに1つitem追加
     """
-    try:
-        dynamodb = boto3.resource("dynamodb")
-        table = dynamodb.Table("python-todoapi")
-        task_id = str(uuid.uuid4())
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table("python-todoapi")
+    task_id = str(uuid.uuid4())
 
-        put_task = {
-            "user_id": user_id,
-            "task_id": task_id,
-            "task_name": task.task_name,
-            "description": task.description,
-            "created_at": datetime.now().strftime("%Y%m%d"),
-            "updated_at": datetime.now().strftime("%Y%m%d"),
-        }
-        table.put_item(Item=put_task)
-        return task_id
-    except BaseException:
-        print(traceback.format_exc())
-        return None
+    put_task = {
+        "user_id": user_id,
+        "task_id": task_id,
+        "task_name": task.task_name,
+        "description": task.description,
+        "created_at": datetime.now().strftime("%Y%m%d"),
+        "updated_at": datetime.now().strftime("%Y%m%d"),
+    }
+    table.put_item(Item=put_task)
+    return task_id
